c2fc6f95207db551e46535ac3eb8e95d51eb40c2
JongHoB <78012131+JongHoB@users.noreply.github.com>
2023-07-11 01:33:49 +0900

    init modify

--- run.c
+++ run.c
@@ -25,14 +25,30 @@ instruction* get_inst_info(uint32_t pc)
     return &INST_INFO[(pc - MEM_TEXT_START) >> 2];
 }
 
-void IF(){//fetch a new instruction from memory
+/***************************************************************/
+/*                                                             */
+/* Procedure: IF                                               */
+/*                                                             */
+/* Purpose:fetch a new instruction from memory                 */
+/*                                                             */
+/***************************************************************/
+void IF(){
 
 
 }
 
-void ID(){//decode the fetched instruction and read the register file
+/***************************************************************/
+/*                                                             */
+/* Procedure: ID                                               */
+/*                                                             */
+/* Purpose: decode the fetched instruction                     */ 
+/*           and read the register file                        */
+/*                                                             */
+/***************************************************************/
+void ID(){
 
 }
+
 /***************************************************************/
 /*                                                             */
 /* Procedure: EX                                               */
@@ -45,10 +61,26 @@ void ID(){//decode the fetched instruction and read the register file
 void EX(){
 
 }
-void MEM(){//access memory for load and store operations
+
+/***************************************************************/
+/*                                                             */
+/* Procedure: MEM                                              */
+/*                                                             */
+/* Purpose: access memory for load and store operationsn       */
+/*                                                             */
+/***************************************************************/
+void MEM(){
 
 }
-void WB(){//write back the result to the register
+
+/***************************************************************/
+/*                                                             */
+/* Procedure: WB                                               */
+/*                                                             */
+/* Purpose: write back the result to the register              */
+/*                                                             */
+/***************************************************************/
+void WB(){
 
 }
 
@@ -61,97 +93,97 @@ void WB(){//write back the result to the register
 /***************************************************************/
 void process_instruction()
 {
-	/** Implement this function */
-    if ((CURRENT_STATE.PC - MEM_TEXT_START) / 4== NUM_INST)
-    {
-        RUN_BIT = FALSE;
-        return;
-    }
+	// /** Implement this function */
+    // if ((CURRENT_STATE.PC - MEM_TEXT_START) / 4== NUM_INST)
+    // {
+    //     RUN_BIT = FALSE;
+    //     return;
+    // }
 
-    instruction *current=get_inst_info(CURRENT_STATE.PC);
-    CURRENT_STATE.PC+=4;
+    // instruction *current=get_inst_info(CURRENT_STATE.PC);
+    // CURRENT_STATE.PC+=4;
 
-    uint32_t jump;
-    switch(OPCODE(current))
-        {
-            //Type I
-            case 0x9:		//(0x001001)ADDIU
-            CURRENT_STATE.REGS[RT(current)]=CURRENT_STATE.REGS[RS(current)]+SIGN_EX(IMM(current));
-            break;
-            case 0xc:		//(0x001100)ANDI
-            CURRENT_STATE.REGS[RT(current)]=CURRENT_STATE.REGS[RS(current)]&IMM(current);
-            break;
-            case 0xf:		//(0x001111)LUI	
-            CURRENT_STATE.REGS[RT(current)]=IMM(current)<<16;
-            break;
-            case 0xd:		//(0x001101)ORI
-            CURRENT_STATE.REGS[RT(current)]=CURRENT_STATE.REGS[RS(current)]|IMM(current);
-            break;
-            case 0xb:		//(0x001011)SLTIU
-            CURRENT_STATE.REGS[RT(current)]=(CURRENT_STATE.REGS[RS(current)] < SIGN_EX(IMM(current))) ? 1 : 0;
-            break;
-            case 0x23:		//(0x100011)LW
-            CURRENT_STATE.REGS[RT(current)] = mem_read_32(CURRENT_STATE.REGS[RS(current)] + SIGN_EX(IMM(current)));
-            break;
-            case 0x2b:		//(0x101011)SW
-            mem_write_32(CURRENT_STATE.REGS[RS(current)] + SIGN_EX(IMM(current)), CURRENT_STATE.REGS[RT(current)]);
-            break;
-            case 0x4:		//(0x000100)BEQ
-            BRANCH_INST(CURRENT_STATE.REGS[RS(current)] == CURRENT_STATE.REGS[RT(current)],CURRENT_STATE.PC+IDISP(current),);
-            break;
-            case 0x5:		//(0x000101)BNE
-            BRANCH_INST(CURRENT_STATE.REGS[RS(current)] != CURRENT_STATE.REGS[RT(current)],CURRENT_STATE.PC+IDISP(current),);
-            break;
+    // uint32_t jump;
+    // switch(OPCODE(current))
+    //     {
+    //         //Type I
+    //         case 0x9:		//(0x001001)ADDIU
+    //         CURRENT_STATE.REGS[RT(current)]=CURRENT_STATE.REGS[RS(current)]+SIGN_EX(IMM(current));
+    //         break;
+    //         case 0xc:		//(0x001100)ANDI
+    //         CURRENT_STATE.REGS[RT(current)]=CURRENT_STATE.REGS[RS(current)]&IMM(current);
+    //         break;
+    //         case 0xf:		//(0x001111)LUI	
+    //         CURRENT_STATE.REGS[RT(current)]=IMM(current)<<16;
+    //         break;
+    //         case 0xd:		//(0x001101)ORI
+    //         CURRENT_STATE.REGS[RT(current)]=CURRENT_STATE.REGS[RS(current)]|IMM(current);
+    //         break;
+    //         case 0xb:		//(0x001011)SLTIU
+    //         CURRENT_STATE.REGS[RT(current)]=(CURRENT_STATE.REGS[RS(current)] < SIGN_EX(IMM(current))) ? 1 : 0;
+    //         break;
+    //         case 0x23:		//(0x100011)LW
+    //         CURRENT_STATE.REGS[RT(current)] = mem_read_32(CURRENT_STATE.REGS[RS(current)] + SIGN_EX(IMM(current)));
+    //         break;
+    //         case 0x2b:		//(0x101011)SW
+    //         mem_write_32(CURRENT_STATE.REGS[RS(current)] + SIGN_EX(IMM(current)), CURRENT_STATE.REGS[RT(current)]);
+    //         break;
+    //         case 0x4:		//(0x000100)BEQ
+    //         BRANCH_INST(CURRENT_STATE.REGS[RS(current)] == CURRENT_STATE.REGS[RT(current)],CURRENT_STATE.PC+IDISP(current),);
+    //         break;
+    //         case 0x5:		//(0x000101)BNE
+    //         BRANCH_INST(CURRENT_STATE.REGS[RS(current)] != CURRENT_STATE.REGS[RT(current)],CURRENT_STATE.PC+IDISP(current),);
+    //         break;
 
-            //TYPE R
-            case 0x0:		//(0x000000)ADDU, AND, NOR, OR, SLTU, SLL, SRL, SUBU  if JR
-            switch(FUNC(current))
-            {
-                case 0x21:
-                CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RS(current)] + CURRENT_STATE.REGS[RT(current)];
-                break;
-                case 0x24:
-                CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RS(current)] & CURRENT_STATE.REGS[RT(current)];
-                break;
-                case 0x08:
-                CURRENT_STATE.PC=CURRENT_STATE.REGS[RS(current)];
-                break;
-                case 0x27:
-                CURRENT_STATE.REGS[RD(current)] = ~(CURRENT_STATE.REGS[RS(current)] | CURRENT_STATE.REGS[RT(current)]);
-                break;
-                case 0x25:
-                CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RS(current)] | CURRENT_STATE.REGS[RT(current)];
-                break;
-                case 0x2B:
-                CURRENT_STATE.REGS[RD(current)] = (CURRENT_STATE.REGS[RS(current)] < CURRENT_STATE.REGS[RT(current)]) ? 1 : 0;
-                break;
-                case 0x00:
-                CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RT(current)] << SHAMT(current);
-                break;
-                case 0x02:
-                CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RT(current)] >> SHAMT(current);
-                break;
-                case 0x23:
-                CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RS(current)] - CURRENT_STATE.REGS[RT(current)];
-                break;
-            }
-            break;
+    //         //TYPE R
+    //         case 0x0:		//(0x000000)ADDU, AND, NOR, OR, SLTU, SLL, SRL, SUBU  if JR
+    //         switch(FUNC(current))
+    //         {
+    //             case 0x21:
+    //             CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RS(current)] + CURRENT_STATE.REGS[RT(current)];
+    //             break;
+    //             case 0x24:
+    //             CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RS(current)] & CURRENT_STATE.REGS[RT(current)];
+    //             break;
+    //             case 0x08:
+    //             CURRENT_STATE.PC=CURRENT_STATE.REGS[RS(current)];
+    //             break;
+    //             case 0x27:
+    //             CURRENT_STATE.REGS[RD(current)] = ~(CURRENT_STATE.REGS[RS(current)] | CURRENT_STATE.REGS[RT(current)]);
+    //             break;
+    //             case 0x25:
+    //             CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RS(current)] | CURRENT_STATE.REGS[RT(current)];
+    //             break;
+    //             case 0x2B:
+    //             CURRENT_STATE.REGS[RD(current)] = (CURRENT_STATE.REGS[RS(current)] < CURRENT_STATE.REGS[RT(current)]) ? 1 : 0;
+    //             break;
+    //             case 0x00:
+    //             CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RT(current)] << SHAMT(current);
+    //             break;
+    //             case 0x02:
+    //             CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RT(current)] >> SHAMT(current);
+    //             break;
+    //             case 0x23:
+    //             CURRENT_STATE.REGS[RD(current)] = CURRENT_STATE.REGS[RS(current)] - CURRENT_STATE.REGS[RT(current)];
+    //             break;
+    //         }
+    //         break;
 
-            //TYPE J
-            case 0x2:		//(0x000010)J
-            jump=((CURRENT_STATE.PC-4)&0xf0000000)+(TARGET(current)<<2);
-            JUMP_INST(jump);
-            break;
-            case 0x3:		//(0x000011)JAL
-            CURRENT_STATE.REGS[31] = CURRENT_STATE.PC + 4;
-            jump=(CURRENT_STATE.PC&0xf0000000)+(TARGET(current)<<2);
-            JUMP_INST(jump);
-            break;
+    //         //TYPE J
+    //         case 0x2:		//(0x000010)J
+    //         jump=((CURRENT_STATE.PC-4)&0xf0000000)+(TARGET(current)<<2);
+    //         JUMP_INST(jump);
+    //         break;
+    //         case 0x3:		//(0x000011)JAL
+    //         CURRENT_STATE.REGS[31] = CURRENT_STATE.PC + 4;
+    //         jump=(CURRENT_STATE.PC&0xf0000000)+(TARGET(current)<<2);
+    //         JUMP_INST(jump);
+    //         break;
 
-            default:
-            break;
+    //         default:
+    //         break;
 
-        }
+    //     }
 
         return;
 }
